d0058767572f32c3763bce61bfe3b29ebdb62793
karangupta99545 <karangupta99545@gmail.com>
2025-12-19 23:09:42 +0530

    Remove emoji from API response message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello API is running 🚀");
+  res.send("Hello API is running ");
 });
 
 const PORT = 80;
